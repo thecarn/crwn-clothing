331c2481740f5a92d799d24d43d2ff96defe8ef6
Mathias Kniesel <kniesel1080@gmail.com>
2020-03-09 21:58:13 -0500

    seperated sign in and sign up components and connected them to DB

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,35 @@ const config = {
     appId: "1:16779371300:web:67136cc52778de4137c9b1",
     measurementId: "G-5Y7QV31NB8"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    console.log(snapShot);
+    console.log('yolo');
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch(error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
