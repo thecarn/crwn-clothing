03d683a597c7bdbb77bddb316710ac24b7ac4dbc
Mathias Kniesel <kniesel1080@gmail.com>
2020-03-13 10:09:08 -0500

    changed styling for signin/signup

--- src/App.js
+++ src/App.js
@@ -30,6 +30,8 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
+
+          console.log(this.state);
         });
       } else {
         this.setState({currentUser: userAuth});
