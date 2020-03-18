6a4d8352a8df7d2f7c98eeca3a9ae63535c6b43b
Mathias Kniesel <kniesel1080@gmail.com>
2020-03-18 05:47:55 -0500

    added styling for hats path & gave gallery images hover animations

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component';
+
 import './collection-item.styles.scss';
 //import CollectionPreview from '../preview-collection/collection-preview.component';
 
@@ -15,6 +17,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+        <CustomButton inverted> Add to cart </CustomButton>
     </div>
 );
 
