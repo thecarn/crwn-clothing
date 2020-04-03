63c8e2159e4f61b779abcc3b88f07725f66c5ffd
Mathias Kniesel <kniesel1080@gmail.com>
2020-04-02 21:07:07 -0500

    checkout page displays cart items & total $

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import './checkout.styles.scss';
 
@@ -24,11 +26,9 @@ const CheckoutPage = ({ cartItems, total }) => (
                 <span>Remove</span>
             </div>
         </div>
-        {
-            cartItems.map(cartItems =>
-                cartItems.name
-            )
-        }
+        {cartItems.map(cartItem => (
+            <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+        ))}
 
         <div className='total'>
             <span>TOTAL: ${total}</span>
